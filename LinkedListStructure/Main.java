4fd9399f4c32006bf7939896618ee4f7ca5ed16d
Ahmet Karaaslanoğlu <92040635+ahmetkaraaslanoglu@users.noreply.github.com>
2022-10-20 00:47:29 +0300

    Update Main.java

--- LinkedListStructure/Main.java
+++ LinkedListStructure/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Single_Linked_List linked_list = new Single_Linked_List(); //
+        Single_Linked_List linked_list = new Single_Linked_List();
 
         linked_list.insert(4);
         linked_list.insert(8);
